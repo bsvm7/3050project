e917313137dc8da96ac7b97cac975db11825dd3b
damnkevin425@gmail.com <ericsong@mwc-037019.dhcp.missouri.edu>
2016-05-03 22:18:36 -0500

    edit solution 1

--- sampleSolution.c
+++ sampleSolution.c
@@ -1,6 +1,7 @@
 #define MAX(a, b) (a)>(b)? (a):(b)
 int maxProfit(int k, int* prices, int size)
 {
+    int i,j;
     if(size < 2)
         return 0;
    if(k > size/2) //when it's unlimited, it can be easily handled -> corner case;
@@ -18,18 +19,25 @@ int maxProfit(int k, int* prices, int size)
         memset(t[i], 0, sizeof(int)*size);
     }
     int cur = 0;
-    for(int i=1; i <= k; i++)
+    for(i=1; i <= k; i++)
     {
         cur = -prices[0];
-        for(int j = 1; j < size; j++)
+        for(j = 1; j < size; j++)
         {
             t[i][j] = MAX(t[i][j-1], prices[j]+cur); //max profit sell at j;
             cur = MAX(cur, t[i-1][j-1]-prices[j]); //max profit buy at j;
+            printf("%d %d current %d\n",i,j,cur);
         }
+    }
+    for(i=0;i<k+1;i++){
+        for(j=0;j<size;j++){
+            
+            printf("%d  ",t[i][j]);
+        }
+        printf("\n");
     }
     return t[k][size-1];
 }
-
 
 #define MAX(a, b) (a)>(b)? (a):(b)
 int maxProfit(int k, int* prices, int size)
